0c61e52f85c09050d808070417147674daee2944
Juan Giovanni Restrepo <grestrepojimenez@gmail.com>
2020-07-24 22:40:20 -0500

    Interfaces, Componentes, Ciclo de vida de componentes , Uso de Inputs ( Property Binding ) y Outputs ( Event Binding ) , Generadores y Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sesion40-angular';
+
+  products: Product[]  = [
+  {
+    id: 1,
+    name: 'Chocorramo',
+    price: 2500,
+    description: 'delicioso chocorramo',
+    image: ''
+  },
+    {
+      id: 2,
+      name: 'Nucita',
+      price: 500,
+      description: 'la nucita es nutritiva y sabrosita....................................................',
+      image: ''
+    },
+    {
+      id: 3,
+      name: 'Glasita',
+      price: 700,
+      description: '',
+      image: ''
+    }
+];
+
+clickProduct(id: number){
+      console.log(`producto ${id} añadido al carrito `);
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './components/product.component';
+import { HeaderComponent } from './header/header.component';
+import { ExponentialPipe } from './exponential.pipe';
+import { FormsModule } from "@angular/forms";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    HeaderComponent,
+    ExponentialPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
